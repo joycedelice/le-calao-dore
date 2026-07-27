fe2c445a4f11f3e0e241023bd450735b24993db2
MAFO JOYCE <joycedelicemafo@gmail.com>
2026-07-27 15:18:40 +0200

    section témoignages avec étoiles dynamiques

--- src/app/components/temoignages/temoignages.ts
+++ src/app/components/temoignages/temoignages.ts
@@ -1,9 +1,45 @@
 import { Component } from '@angular/core';
-
+interface Testimonial { 
+  id: number; 
+  name: string; 
+  rating: number; 
+  text: string; 
+}
 @Component({
   selector: 'app-temoignages',
   imports: [],
   templateUrl: './temoignages.html',
   styleUrl: './temoignages.css',
+
+  
 })
-export class Temoignages {}
+  
+  
+export class Temoignages { 
+  testimonials: Testimonial[] = [  
+    { 
+      id: 1, 
+      name: 'Aïssatou M.', 
+      rating: 5, 
+      text: 'Le Ndolè est le meilleur de Douala. Service impeccable.' 
+    }, 
+    { 
+      id: 2, 
+      name: 'Jean-Pierre K.', 
+      rating: 4, 
+      text: 'Ambiance chaleureuse et plats généreux. Je recommande.' 
+    }, 
+    { 
+      id: 3, 
+      name: 'Christelle E.', 
+      rating: 5, 
+      text: 'Cadre magnifique, cuisine authentique, prix raisonnables.' 
+    } 
+  ]; 
+  getStars(rating: number): number[] { 
+
+    return Array(rating).fill(0);
+  }  
+  
+} 
+
